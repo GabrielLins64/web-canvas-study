0b3b1445bfaa821d8a02eb79c85355bf05e2762a
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-09-10 18:26:18 -0300

    fix paddle's ball and wall collisions

--- breakout/js/physics.js
+++ breakout/js/physics.js
@@ -1,15 +1,18 @@
-function detectWallCollision(canvas, object) {
-  if ((object.y + object.dy < object.size) || (object.y + object.dy > canvas.height - object.size))
-    object.dy = -object.dy;
-  else if ((object.x + object.dx < object.size) || (object.x + object.dx > canvas.width - object.size))
-    object.dx = -object.dx;
+function detectWallCollision(canvas, ball, paddle) {
+  if ((ball.y + ball.dy < ball.size))
+    ball.dy = -ball.dy;
+  else if ((ball.x + ball.dx < ball.size) || (ball.x + ball.dx > canvas.width - ball.size))
+    ball.dx = -ball.dx;
+
+  if (paddle.x + paddle.width > canvas.width) paddle.x -= paddle.dx
+  else if (paddle.x < 0) paddle.x += paddle.dx
 }
 
 function detectBallPaddleCollision(ball, paddle) {
-  if ((ball.y + ball.dy + ball.size > paddle.y)
-    && (ball.x + ball.size < paddle.x + paddle.width)
-    && (ball.x - ball.size > paddle.x))
-    ball.dy = -ball.dy;
+  if ((ball.y + ball.dy + ball.size == paddle.y)
+    && (ball.x - ball.size / 2 < paddle.x + paddle.width)
+    && (ball.x + ball.size / 2 > paddle.x))
+    ball.dy = -Math.abs(ball.dy);
 }
 
 export { detectWallCollision, detectBallPaddleCollision }
