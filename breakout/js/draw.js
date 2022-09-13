cb44cc97a2a5f27326b5fd120baae16b9b5d78ca
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-09-13 18:32:27 -0300

    add score and win check

--- breakout/js/draw.js
+++ breakout/js/draw.js
@@ -8,6 +8,12 @@ function drawRect(ctx, x=0, y=0, width=10, height=10, color="#000") {
   ctx.closePath();
 }
 
+function drawScore(ctx, score) {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#eeff00";
+  ctx.fillText(`Score: ${score.at(0)}`, 8, 20);
+}
+
 function drawBall(ctx, centerX=0, centerY=0, radius=10, color="#000") {
   ctx.beginPath();
   ctx.arc(centerX, centerY, radius, 0, Math.PI * 2, false);
@@ -20,4 +26,4 @@ const clearCanvas = (canvas, ctx) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-export { drawRect, drawBall, clearCanvas }
+export { drawRect, drawBall, clearCanvas, drawScore }
